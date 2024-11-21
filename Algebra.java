735c76ad38124c48692d60b141c68604bacc7978
Neta Fruchter <netafruchter@Netas-MacBook-Pro.local>
2024-11-21 18:52:26 +0200

    first try

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,111 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 < 0) {
+			for (int i = 0; i < (-x2); i++){
+				x1--;
+			}
+		}
+		else {
+			for (int i = 0; i < x2; i++){
+				x1++;
+			}
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 < 0){
+			for (int i = 0; i < (-x2); i++){
+				x1++;
+			}
+		}
+		else{
+			for (int i = 0; i < x2; i++) {
+				x1--;
+			}
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int x3 = x1;
+		if (x2 < 0){
+			for (int i = 1; i < minus(0,x2); i++){
+				x3 = plus(x3, x1);
+			}
+		}
+		else{
+			for (int i = 1; i < x2; i++){
+				x3 = plus(x3, x1);
+			}
+		}
+		return x3;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int x2 = x;
+		for (int i = 1; i < n; i++){
+			x2 = times(x2, x);
+		}
+		return x2;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 == 0) {
+			System.out.println("cannot divide by 0");
+			return 0;
+		}
+		int x3 = x1;
+		int counter = 0;
+
+		if(x1 >= 0 && x2 > 0) {
+			while (!(x3 <= 0)) {
+				x3 = minus(x3, x2);
+				if (x3>=0){
+					counter++;
+				}
+			}
+		}
+		else {
+			while (!(x3 >= 0)) {
+				x3 = minus(x3, x2);
+				if 	(x3<=0){
+					counter++;
+				}
+			}
+			if(x1 <= 0 || x2 < 0){
+				counter = -counter;
+			}
+		}
+		return counter;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int x3 = div(x1,x2);
+		int rest = minus(x1,times(x2, x3));
+		return rest;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int i = 0;
+		
+		while (true) {
+			if (times(i,i) == x){
+				break;
+			}
+			else if(times(i,i) > x) {
+				i--;
+				break;
+			}
+			i++;
+		}
+		return i;
 	}	  	  
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -14,6 +14,8 @@ public class LoanCalc {
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
+		System.out.println("the end balace using this terms is: " + endBalance(loan, rate, n, 10000));
+
 		// Computes the periodical payment using brute force search
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
@@ -23,13 +25,17 @@ public class LoanCalc {
 		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
+
 	}
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		double balance = loan;
+		for (int i=0; i < n; i++) {
+			balance = (balance - payment) * (1+(rate/100));
+		}
+		return balance;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +44,14 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double g = loan/n;
+		iterationCounter = 0;
+
+		while (endBalance(loan, rate, n, g) > 0){
+			g+=epsilon;
+			iterationCounter++;
+		}
+		return g;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +60,20 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
-    }
+        double L= loan / n;
+		double H = loan;
+		double g = (L+H) / 2;
+
+		iterationCounter = 0;
+		while((H - L) > epsilon){
+			if (endBalance(loan, rate, n, g) * endBalance(loan, rate, n, L) > 0){
+				L = g;
+			} else {
+				H = g;
+			}
+			iterationCounter++;
+			g = (L + H) / 2;
+		}
+		return g;
+	}
 }
